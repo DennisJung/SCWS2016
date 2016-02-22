4b1301e66663e756a1be6a3d688af86071915886
DennisJung <hyunsukdn@gmail.com>
2016-02-22 21:17:45 +0900

    git add

---
+++ imgdiff/imgdiff.h
@@ -0,0 +1,10 @@
+
+
+#ifndef __IMGDIFF_H__
+#define __IMGDIFF_H__
+
+// Imgdiff algorighm
+void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigned char* images);
+
+#endif // __IMGDIFF_H__
+
