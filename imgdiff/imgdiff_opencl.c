afa46f634d85d510166361c4750136f326641070
DennisJung <hyunsukdn@gmail.com>
2016-02-22 21:18:25 +0900

    file add

---
+++ imgdiff/imgdiff_opencl.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+#include "imgdiff.h"
+
+void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigned char* images) {
+}
+
---
+++ imgdiff/libjpeg_wrapper.h
@@ -0,0 +1,9 @@
+#ifndef __LIBJPEG_WRAPPER_H__
+#define __LIBJPEG_WRAPPER_H__
+
+#include <jpeglib.h>
+
+void open_jpeg_image(const char* filename, unsigned char* image);
+
+#endif // __LIBJPEG_WRAPPER_H__
+
