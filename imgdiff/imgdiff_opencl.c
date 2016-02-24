480904c60068fb72575994ae56f24660a9dbe05f
DennisJung <hyunsukdn@gmail.com>
2016-02-24 15:30:10 +0900

    dummy data run

--- imgdiff/imgdiff_opencl.c
+++ imgdiff/imgdiff_opencl.c
@@ -3,6 +3,80 @@
 #include <CL/cl.h>
 #include "imgdiff.h"
 
+static const char* TranslateOpenCLError(cl_int errorCode)
+{
+	switch (errorCode)
+	{
+		case CL_SUCCESS:                            return "CL_SUCCESS";
+		case CL_DEVICE_NOT_FOUND:                   return "CL_DEVICE_NOT_FOUND";
+		case CL_DEVICE_NOT_AVAILABLE:               return "CL_DEVICE_NOT_AVAILABLE";
+		case CL_COMPILER_NOT_AVAILABLE:             return "CL_COMPILER_NOT_AVAILABLE";
+		case CL_MEM_OBJECT_ALLOCATION_FAILURE:      return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+		case CL_OUT_OF_RESOURCES:                   return "CL_OUT_OF_RESOURCES";
+		case CL_OUT_OF_HOST_MEMORY:                 return "CL_OUT_OF_HOST_MEMORY";
+		case CL_PROFILING_INFO_NOT_AVAILABLE:       return "CL_PROFILING_INFO_NOT_AVAILABLE";
+		case CL_MEM_COPY_OVERLAP:                   return "CL_MEM_COPY_OVERLAP";
+		case CL_IMAGE_FORMAT_MISMATCH:              return "CL_IMAGE_FORMAT_MISMATCH";
+		case CL_IMAGE_FORMAT_NOT_SUPPORTED:         return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+		case CL_BUILD_PROGRAM_FAILURE:              return "CL_BUILD_PROGRAM_FAILURE";
+		case CL_MAP_FAILURE:                        return "CL_MAP_FAILURE";
+		case CL_MISALIGNED_SUB_BUFFER_OFFSET:       return "CL_MISALIGNED_SUB_BUFFER_OFFSET";                              //-13
+		case CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST:    return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_L    IST";   //-14
+								      /*
+									 33                                                                  case CL_COMPILE_PROGRAM_FAILURE:            re    turn "CL_COMPILE_PROGRAM_FAILURE";                               //-15
+									 34                                                                  case CL_LINKER_NOT_AVAILABLE:               re    turn "CL_LINKER_NOT_AVAILABLE";                                  //-16
+									 35                                                                  case CL_LINK_PROGRAM_FAILURE:               re    turn "CL_LINK_PROGRAM_FAILURE";                                  //-17
+									 36                                                                  case CL_DEVICE_PARTITION_FAILED:            re    turn "CL_DEVICE_PARTITION_FAILED";                               //-18
+									 37                                                                  case CL_KERNEL_ARG_INFO_NOT_AVAILABLE:      re    turn "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";                         //-19
+									 38                                                                */
+		case CL_INVALID_VALUE:                      return "CL_INVALID_VALUE";
+		case CL_INVALID_DEVICE_TYPE:                return "CL_INVALID_DEVICE_TYPE";
+		case CL_INVALID_PLATFORM:                   return "CL_INVALID_PLATFORM";
+		case CL_INVALID_DEVICE:                     return "CL_INVALID_DEVICE";
+		case CL_INVALID_CONTEXT:                    return "CL_INVALID_CONTEXT";
+		case CL_INVALID_QUEUE_PROPERTIES:           return "CL_INVALID_QUEUE_PROPERTIES";
+		case CL_INVALID_COMMAND_QUEUE:              return "CL_INVALID_COMMAND_QUEUE";
+		case CL_INVALID_HOST_PTR:                   return "CL_INVALID_HOST_PTR";
+		case CL_INVALID_MEM_OBJECT:                 return "CL_INVALID_MEM_OBJECT";
+		case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:    return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+		case CL_INVALID_IMAGE_SIZE:                 return "CL_INVALID_IMAGE_SIZE";
+		case CL_INVALID_SAMPLER:                    return "CL_INVALID_SAMPLER";
+		case CL_INVALID_BINARY:                     return "CL_INVALID_BINARY";
+		case CL_INVALID_BUILD_OPTIONS:              return "CL_INVALID_BUILD_OPTIONS";
+		case CL_INVALID_PROGRAM:                    return "CL_INVALID_PROGRAM";
+		case CL_INVALID_PROGRAM_EXECUTABLE:         return "CL_INVALID_PROGRAM_EXECUTABLE";
+		case CL_INVALID_KERNEL_NAME:                return "CL_INVALID_KERNEL_NAME";
+		case CL_INVALID_KERNEL_DEFINITION:          return "CL_INVALID_KERNEL_DEFINITION";
+		case CL_INVALID_KERNEL:                     return "CL_INVALID_KERNEL";
+		case CL_INVALID_ARG_INDEX:                  return "CL_INVALID_ARG_INDEX";
+		case CL_INVALID_ARG_VALUE:                  return "CL_INVALID_ARG_VALUE";
+		case CL_INVALID_ARG_SIZE:                   return "CL_INVALID_ARG_SIZE";
+		case CL_INVALID_KERNEL_ARGS:                return "CL_INVALID_KERNEL_ARGS";
+		case CL_INVALID_WORK_DIMENSION:             return "CL_INVALID_WORK_DIMENSION";
+		case CL_INVALID_WORK_GROUP_SIZE:            return "CL_INVALID_WORK_GROUP_SIZE";
+		case CL_INVALID_WORK_ITEM_SIZE:             return "CL_INVALID_WORK_ITEM_SIZE";
+		case CL_INVALID_GLOBAL_OFFSET:              return "CL_INVALID_GLOBAL_OFFSET";
+		case CL_INVALID_EVENT_WAIT_LIST:            return "CL_INVALID_EVENT_WAIT_LIST";
+		case CL_INVALID_EVENT:                      return "CL_INVALID_EVENT";
+		case CL_INVALID_OPERATION:                  return "CL_INVALID_OPERATION";
+		case CL_INVALID_GL_OBJECT:                  return "CL_INVALID_GL_OBJECT";
+		case CL_INVALID_BUFFER_SIZE:                return "CL_INVALID_BUFFER_SIZE";
+		case CL_INVALID_MIP_LEVEL:                  return "CL_INVALID_MIP_LEVEL";
+		case CL_INVALID_GLOBAL_WORK_SIZE:           return "CL_INVALID_GLOBAL_WORK_SIZE";                               //-63
+		case CL_INVALID_PROPERTY:                   return "CL_INVALID_PROPERTY";                                       //-64
+							    /*
+							       75                                                        case CL_INVALID_IMAGE_DESCRIPTOR:           return "CL_I    NVALID_IMAGE_DESCRIPTOR";                           //-65
+							       76                                                        case CL_INVALID_COMPILER_OPTIONS:           return "CL_I    NVALID_COMPILER_OPTIONS";                           //-66
+							       77                                                        case CL_INVALID_LINKER_OPTIONS:             return "CL_I    NVALID_LINKER_OPTIONS";                             //-67
+							       78                                                        case CL_INVALID_DEVICE_PARTITION_COUNT:     return "CL_I    NVALID_DEVICE_PARTITION_COUNT";                     //-68
+							       79                                                      */
+							    //    case CL_INVALID_PIPE_SIZE:                  return "C    L_INVALID_PIPE_SIZE";                                  //-69
+							    //    case CL_INVALID_DEVICE_QUEUE:               return "C    L_INVALID_DEVICE_QUEUE";                               //-70
+		default:
+							    return "UNKNOWN ERROR CODE";
+	}
+}
+
 
 int ReadSourceFromFile(const char* fileName, char** source, size_t* sourceSize)
 {
@@ -49,7 +123,8 @@ void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigne
 	cl_uint num_platforms;
 	cl_uint num_devs;
 
-	cl_mem* m_image;
+	cl_mem* m_image1;
+	cl_mem* m_image2;
 	cl_mem* m_result;
 
 	cl_event* ev_kernels;
@@ -82,7 +157,7 @@ void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigne
 		return 0;
 	}
 
-	for(i = 0; i<num_devs; i++)
+	for(i = 0; i<num_platforms; i++)
 	{
 		err = clGetDeviceIDs(platform[i], dev_type, 0, NULL, &num_devs);
 		if(err != CL_SUCCESS)
@@ -99,7 +174,7 @@ void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigne
 		}
 	}
 
-	printf("Device Get\n");
+	printf("Device Count %d\n", num_devs);
 
 	context = clCreateContext(NULL, num_devs, devs, NULL, NULL, &err);
 	if(err != CL_SUCCESS)
@@ -161,74 +236,111 @@ void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigne
 	}
 
 	printf("Create commandQueue Success\n");
-
-	int WORK_WIDTH = width;
-	int WORK_HEIGHT = height;
-	int WORK_AMOUNT = WORK_WIDTH * WORK_HEIGHT;
-	printf("WORK_WIDTH %d\tWORK_HEIGHT %d\t WORK_AMOUNT %d\n", WORK_WIDTH, WORK_HEIGHT, WORK_AMOUNT);
+	int LOCAL_WIDTH = 16;
+	int LOCAL_HEIGHT = 16;
+	int WORK_WIDTH = ceil((double)width / LOCAL_WIDTH)*LOCAL_WIDTH;
+	int WORK_HEIGHT = ceil((double)height/LOCAL_HEIGHT) * LOCAL_HEIGHT / 4;
+	int WORK_AMOUNT = width * height / 4;
+	int WORK_GROUP_COUNT = WORK_WIDTH * WORK_HEIGHT / (LOCAL_WIDTH * LOCAL_HEIGHT);
+	printf("WORK_WIDTH %d\tWORK_HEIGHT %d\t WORK_AMOUNT %d\t WORK_GROUP_COUNT %d\n", 
+		WORK_WIDTH, WORK_HEIGHT, WORK_AMOUNT, WORK_GROUP_COUNT);
 	
-	m_image = (cl_mem*)malloc(sizeof(cl_mem)* num_devs);
+	m_image1 = (cl_mem*)malloc(sizeof(cl_mem)* num_devs);
+	m_image2 = (cl_mem*)malloc(sizeof(cl_mem)* num_devs);
 	m_result = (cl_mem*)malloc(sizeof(cl_mem)* num_devs);
 
 
 	for(i=0; i<num_devs; i++)
 	{
-		m_image[i] = clCreateBuffer(context, CL_MEM_READ_ONLY, WORK_AMOUNT * sizeof(unsigned char)*3, NULL, NULL);
-		m_result[i] = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(double), NULL, NULL);
+		m_image1[i] = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char) * WORK_AMOUNT, NULL, NULL);
+		m_image2[i] = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(unsigned char) * WORK_AMOUNT, NULL, NULL);
+		
+		m_result[i] = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(double) * WORK_AMOUNT, NULL, NULL);
 	}
 
 	printf("Create Buffer Success\n");
 
 	for(i=0; i<num_devs; i++)
 	{		
-		clSetKernelArg(kernels[i], 0, sizeof(cl_mem), (void*)&m_image[i]);
-		clSetKernelArg(kernels[i], 1, sizeof(cl_mem), (void*)&m_result[i]);
+		clSetKernelArg(kernels[i], 0, sizeof(cl_mem), (void*)&m_image1[i]);
+		clSetKernelArg(kernels[i], 1, sizeof(cl_mem), (void*)&m_image2[i]);
+		clSetKernelArg(kernels[i], 2, sizeof(cl_mem), (void*)&m_result[i]);
 	}
 
 	printf("Set Kernel Arguments\n");
-	/*
-	int row, col;
-	for(row = 0; row < N; row++)
-	{
-		for(col=0; col<)
-	*/	
-	size_t gws[2] = { WORK_WIDTH, WORK_HEIGHT };
 	
-	size_t lws[2] = { 256, 256 };
-
 	ev_kernels  = (cl_event*)malloc(sizeof(cl_event)*num_devs);
 	
-	for(i=0; i<num_devs; i++)
-	{
-
-		clEnqueueWriteBuffer(cmd_queues[i], m_image[i], CL_FALSE, 0, WORK_AMOUNT*sizeof(unsigned char)*3, (void*)(images + i * WORK_AMOUNT), 0, NULL, NULL);
-
-	}
-
-	printf("Write Buffer Success\n");
-
-	for( i=0; i < num_devs; i++ )
-	{
-
-		err = clEnqueueNDRangeKernel(cmd_queues[i], kernels[i], 2, NULL, gws, lws, 0, NULL, NULL);
-	}
-
-
-	diff_matrix[0] = 0;	
-	for( i =0; i < num_devs; i++ )
-	{
-		clEnqueueReadBuffer( cmd_queues[i], m_result[i], CL_TRUE, 0, sizeof(double), diff_matrix + i, 0, NULL, NULL); 
-	}
 	
-	printf("%lf %lf %lf %lf\n", diff_matrix[0], diff_matrix[1], diff_matrix[2], diff_matrix[3]);
-	diff_matrix[0] = 2;
-
-	diff_matrix[1] = 2;
 	
-	diff_matrix[2] = 2;
+	int row, col;
+	
+	row = 0;
+	col = 1;
+	//for(row = 0; row < N; row++)
+	//{
+		diff_matrix[row*N + row] = 0;
+		for(i=0; i<num_devs; i++)
+		{
+
+			clEnqueueWriteBuffer(cmd_queues[i], m_image1[i], CL_TRUE, 0, 
+				WORK_AMOUNT*sizeof(unsigned char)*3, (void*)(images + 
+				((row * width*height) + (WORK_AMOUNT * i))), 0, NULL, NULL);
+
+		}
+		//for(col=row+1; col< N; col++)
+		//{
+
+
+			size_t gws[2] = { WORK_WIDTH, WORK_HEIGHT};
+
+			size_t lws[2] = { 16, 16 };
+
+
+			for(i=0; i<num_devs; i++)
+			{
+
+				clEnqueueWriteBuffer(cmd_queues[i], m_image2[i], CL_TRUE, 0, 
+					WORK_AMOUNT*sizeof(unsigned char)*3, (void*)(images + 
+					((col * width*height) + (WORK_AMOUNT * i))), 0, NULL, NULL);
+
+
+			}
+
+			//printf("Write Buffer Success\n");
+
+			for( i=0; i < num_devs; i++ )
+			{
+
+				err = clEnqueueNDRangeKernel(cmd_queues[i], kernels[i], 2, NULL, gws, lws, 0, NULL, NULL);
+				if(err != CL_SUCCESS)
+				{
+					printf("Error: clEnqueueNDRangeKernel %d error\n", i);
+					printf("%s\n", TranslateOpenCLError(err));
+					return 0;
+				}
+			}
+
+			
+			for( i =0; i < num_devs; i++ )
+			{
+				err = clEnqueueReadBuffer( cmd_queues[i], m_result[i], CL_TRUE, 0, sizeof(double), diff_matrix + i, 0, NULL, NULL); 
+				if(err != CL_SUCCESS)
+				{
+					printf("Error: clEnqueueReadBuffer%d error\n", i);
+					return 0;
+				}
+			}
+
+			//printf("%lf %lf %lf %lf\n", diff_matrix[0], diff_matrix[1], diff_matrix[2], diff_matrix[3]);
+		//}
+	//}
+	/*
 	for( i =0; i < num_devs; i++)
 	{
-		clReleaseMemObject( m_image[i] );
+		printf("test%d\n", i);
+		clReleaseMemObject( m_image1[i] );	
+		clReleaseMemObject( m_image2[i] );
 		clReleaseMemObject( m_result[i] );
 		clReleaseKernel(kernels[i]);
 		clReleaseCommandQueue(cmd_queues[i]);
@@ -238,12 +350,14 @@ void imgdiff(size_t N, size_t width, size_t height, double* diff_matrix, unsigne
 	clReleaseProgram(program);
 	clReleaseContext(context);
 	free(platform);
-	free(m_image);
+	free(m_image1);
+	free(m_image2);
 	free(m_result);
 	free(cmd_queues);
 	free(kernels);
 	free(devs);
 	free(ev_kernels);
+	*/
 	printf("OpenCL End\n");
 
 }
